1ee7e67eea6059a01757909959d7e5779131ca1d
pmqueiroz <pedroqueiroz593@gmail.com>
2022-02-01 09:00:15 -0300

    fix: remove horizontal overflow

--- src/hocs/withNavigation/index.tsx
+++ src/hocs/withNavigation/index.tsx
@@ -4,19 +4,27 @@ import styled from 'styled-components'
 import { Navigation } from '../../components'
 import { theme } from '../../styles/theme'
 
-const Wrapper = styled.main`
+const PageWrapper = styled.main`
   display: flex;
   min-height: calc(100vh - ${theme.global.sizes.navigationHeight});
 `
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    overflow-x: hidden;
+`
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const withNavigation = (Page: NextPage<any>) => {
     function WrappedPage(props: unknown) {
         return (
-            <>
+            <Wrapper>
                 <Navigation />
-                <Wrapper>
+                <PageWrapper>
                     <Page {...props} />
-                </Wrapper>
-            </>
+                </PageWrapper>
+            </Wrapper>
         )
     }
 
