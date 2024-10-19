b8de704ee43f3196618fde1bf9312dcbcbf91404
pmqueiroz <pedroqueiroz593@gmail.com>
2024-10-19 12:20:41 -0300

    chore: start project from scratch

---
+++ apps/portfolio/next.config.js
@@ -0,0 +1,18 @@
+//@ts-check
+
+const { composePlugins, withNx } = require('@nx/next');
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    svgr: false,
+  },
+};
+
+const plugins = [
+  withNx,
+];
+
+module.exports = composePlugins(...plugins)(nextConfig);
---
+++ apps/portfolio/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      🚧 under maintenance 🚧
+    </div>
+  );
+}
